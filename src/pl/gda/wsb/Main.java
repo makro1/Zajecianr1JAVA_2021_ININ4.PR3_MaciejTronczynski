8fd58c73a76d76fdb4bae878f11c3be220dc2f90
makro1 <termicab@gmail.com>
2021-02-28 17:43:55 +0100

    Humans saved to txt with FileWriter

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -1,10 +1,12 @@
 package pl.gda.wsb;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         Animal animal1 = new Animal("Pies", 2, 50, true);
@@ -17,19 +19,19 @@ public class Main {
         Human human2 = new Human("Mariola", 50, 155);
         Human human3 = new Human("Julek", 50, 170);
 
-        if(animal1.height > animal2.height){
+        if (animal1.height > animal2.height) {
             System.out.println(animal1.name + " jest wyższy od " + animal2.name);
         }
 
-        if(human2.height > human1.height) {
+        if (human2.height > human1.height) {
             System.out.println(human2.name + " jest wyższy/a od " + human1.name);
-        }else {
+        } else {
             System.out.println(human1.name + " jest wyższy/a od " + human2.name);
         }
 
         // StringBuilder humanWithMaxHeightDescription = new StringBuilder()
-          //      .append("The tallest person: ")
-            //    .append(humanWithMaxHeight.name)
+        //      .append("The tallest person: ")
+        //    .append(humanWithMaxHeight.name)
 
 
         System.out.println();
@@ -41,15 +43,13 @@ public class Main {
         for (Human human : humansList) {
             System.out.println(human.name + " " + human.age + " " + human.height);
 
+            FileWriter fw = new FileWriter("human.txt", true);
+            fw.write(human.name + " " + human.age + " " + human.age + "\n");
+            fw.close();
+
         }
 
 
-
-
-
-
-
-        
-	// write your code here
+        // write your code here
     }
 }
